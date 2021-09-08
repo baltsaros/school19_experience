38fe1bf1d76c6a04f7b890ccc85d07176c16af03
abuzdin <abuzdin@student.s19.be>
2021-09-08 17:24:41 +0000

    rename for bc01 into bc00

---
+++ bc00/ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,23 @@
+#include <unistd.h>
+
+void	ft_putchar(char z)	
+{
+	write(1, &z, 1);
+}
+
+void ft_print_alphabet(void)
+{
+	char	z;
+	z = 'z';
+	while (z >= 'a')
+	{
+		ft_putchar(z);
+		z--;
+	}
+}
+
+int     main()
+{
+    ft_print_alphabet();
+    return 0;
+}
---
+++ bc00/test.c
@@ -0,0 +1,23 @@
+#include <unistd.h>
+
+void	ft_putchar(char n)	
+{
+	write(1, &n, 1);
+}
+
+void ft_print_alphabet(void)
+{
+	char	n;
+	n = 48;
+	while (n <= '9')
+	{
+		ft_putchar(n);
+		n++;
+	}
+}
+
+int     main()
+{
+    ft_print_alphabet();
+    return 0;
+}
