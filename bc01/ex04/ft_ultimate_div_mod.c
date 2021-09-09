d6de79753501e102d41fa08c6abdaebf8679caf7
abuzdin <abuzdin@student.s19.be>
2021-09-09 13:31:02 +0000

    ex04 is done

--- bc01/ex04/ft_ultimate_div_mod.c
+++ bc01/ex04/ft_ultimate_div_mod.c
@@ -2,21 +2,22 @@
 
 void	ft_ultimate_div_mod(int *a, int *b)
 {
-	printf("%d\n", *a);
-	printf("%d\n", *b);
+	int div;
+	int mod;
+
+	div = a / b;
+	mod = a % b;
+	*a = div;
+	*b = mod;
 }
 
 int main()
 {
 	int a;
 	int b;
-	int div;
-	int mod;
-	
+
 	a = 44;
 	b = 5;
-	div = a / b;
-	mod = a % b;
 	ft_ultimate_div_mod(&div, &mod);
 	return 0;
 }
