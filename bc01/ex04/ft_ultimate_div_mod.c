cb7bf6214fe276fa596830290ca047f1ed32c5fe
abuzdin <abuzdin@student.s19.be>
2021-09-09 14:50:19 +0000

    ex04 update

--- bc01/ex04/ft_ultimate_div_mod.c
+++ bc01/ex04/ft_ultimate_div_mod.c
@@ -5,8 +5,8 @@ void	ft_ultimate_div_mod(int *a, int *b)
 	int		div;
 	int		mod;
 
-	div = a / b;
-	mod = a % b;
+	div = *a / *b;
+	mod = *a % *b;
 	*a = div;
 	*b = mod;
 }
@@ -18,7 +18,8 @@ int main()
 
 	a = 44;
 	b = 5;
+	printf("a(initial) %d, b(initial) %d\n", a, b);
 	ft_ultimate_div_mod(&a, &b);
-	printf("a(div) %d, b(mod) %d", a, b);
+	printf("a(div) %d, b(mod) %d\n", a, b);
 	return 0;
 }
