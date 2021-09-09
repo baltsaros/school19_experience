2b83f9d3f2ea830e2a768bdb6d671083af74a822
abuzdin <abuzdin@student.s19.be>
2021-09-09 06:34:56 +0000

    folders update

---
+++ bc01/ex01/ft_ultimate_ft.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+void	ft_ultimate_ft(int *********nbr)
+{
+	printf("%d\n", *********nbr);
+}
+
+void	ft_ft(int nbr)
+{
+	int		*ptr;
+	int		**ptr1;
+	int		***ptr2;
+	int		****ptr3;
+	int		*****ptr4;
+	int		******ptr5;
+	int		*******ptr6;
+	int		********ptr7;
+
+	ptr = &nbr;
+	ptr1 = &ptr;
+	ptr2 = &ptr1;
+	ptr3 = &ptr2;
+	ptr4 = &ptr3;
+	ptr5 = &ptr4;
+	ptr6 = &ptr5;
+	ptr7 = &ptr6;
+	nbr = &ptr7;
+
+}
+
+int main()
+{
+	ft_ultimate_ft(42);
+	return 0;
+}
