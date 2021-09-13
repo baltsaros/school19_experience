87f9b9b46f1124d3119b056ffc5b6d12a9003ed2
abuzdin <abuzdin@student.s19.be>
2021-09-13 19:35:12 +0000

    ex00 is done

---
+++ bc03/ex00/ft_strcmp.c
@@ -0,0 +1,35 @@
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] > s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+		if (s1[i] < s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+		if (s1[i] == s2[i])
+		{
+			++i;
+		}
+	}
+	return (0);
+}
+
+#include <stdio.h>
+int	main()
+{
+	char	s1[] = " bc";
+	char	s2[] = "bBc";
+	int	d;
+
+	ft_strcmp(s1, s2);
+	d = ft_strcmp(s1, s2);
+	printf("return is %d\n", d);
+	return (0);
+}
