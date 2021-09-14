8801b800f8779f8f2493777c5e556cab6447312c
abuzdin <abuzdin@student.s19.be>
2021-09-14 12:22:47 +0000

    ex04 is done

--- bc03/ex04/ft_strstr.c
+++ bc03/ex04/ft_strstr.c
@@ -1,13 +1,52 @@
+int	ft_strlen(char *dest)
+{
+	int		i;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		++i;
+	}
+	return (i);
+}
+
 char	*ft_strstr(char *str, char *to_find)
 {
+	int	i;
+	int	j;
+	int	size_f;
 
+	size_f = ft_strlen(to_find);
+	i = 0;
+	j = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] == to_find[j])
+		{
+			++i;
+			++j;
+			if (j == size_f)
+			{
+				return ("Got it");
+			}
+		}
+		else
+		{
+			++i;
+			j = 0;
+		}
+	}
+	return (0);
 }
 
 #include <stdio.h>
 int	main()
 {
-	char	str[] = "";
-	char	to_find[] = "";
+	char	str[] = "hayNeedl";
+	char	to_find[] = "Needle";
+	char	*ret;
 
-	
+	ret = ft_strstr(str, to_find);
+	printf("return is %s\n", ret);
+	return (0);
 }
