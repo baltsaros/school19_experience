d26f9978993c976a9bf2d0f091663694e80ec150
abuzdin <abuzdin@student.s19.be>
2021-09-14 10:57:28 +0000

    ex03 is done

---
+++ bc03/ex03/ft_strncat.c
@@ -0,0 +1,49 @@
+int	ft_strlen(char *dest)
+{
+	int i;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		++i;
+	}
+	return (i);
+}
+
+char	*ft_strncat(char *dest, char *src, unsigned int nb)
+{
+	unsigned int	j;
+	int	size_d;
+	int	size_s;
+
+	j = 0;
+	size_d = ft_strlen(dest);
+	size_s = ft_strlen(src);
+	while (src[j] != '\0' && j < nb)
+	{
+		dest[size_d] = src[j];
+		++j;
+		++size_d;
+	}
+	while (j < nb)
+	{
+		dest[size_d] = '\0';
+		++size_d;
+		++j;
+	}
+	return (dest);
+}
+
+#include <stdio.h>
+#include <string.h>
+int	main()
+{
+	char	dest[] = "Hello";
+	char	src[] = " world";
+	unsigned int	nb;
+
+	nb = 5;
+	printf("initial dest is %s\n", dest);
+	ft_strncat(dest, src, nb);
+	printf("concatenated dest is %s\n", dest);
+}
