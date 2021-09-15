07c4906edca82cec53b9d7bc3946f70ce6b80c30
abuzdin <abuzdin@student.s19.be>
2021-09-15 20:34:17 +0000

    ex03 is done

---
+++ bc04/ex03/ft_atoi.c
@@ -0,0 +1,56 @@
+int	ft_sign(char *str)
+{
+	int		i;
+	int		sign;
+
+	i = 0;
+	sign = 0;
+	while (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i]== '-')
+			++sign;
+		++i;
+	}
+	if (sign / 2 != 0)
+		return (-i);
+	else
+		return(i);
+}
+
+int	ft_atoi(char *str)
+{
+	int		i;
+	int		j;
+	int		sign;
+	int		tab;
+
+	i = 0;
+	tab = 0;
+	if (ft_sign(str) >= 0)
+	{
+		j = ft_sign(str);
+		sign = 1;
+	}
+	else
+	{
+		j = -ft_sign(str);
+		sign = -1;
+	}
+	while (str[j] >= 48 && str[j] <= 57)
+	{
+		tab = tab * 10 + str[j] - '0';
+		++i;
+		++j;
+	}
+	tab = tab * sign;
+	return (tab);
+}
+
+#include <stdio.h>
+int	main()
+{
+	char	str[] = "---+--+1234ab567";
+
+	printf("%d\n", ft_atoi(str));
+	return (0);
+}
