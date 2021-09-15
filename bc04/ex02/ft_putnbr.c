dc4e34d7e1979da2c65bbb50dafbe897e37eab25
abuzdin <abuzdin@student.s19.be>
2021-09-15 18:44:22 +0000

    ex02 is done

--- bc04/ex02/ft_putnbr.c
+++ bc04/ex02/ft_putnbr.c
@@ -9,10 +9,34 @@ void	ft_putnbr(int nb)
 {
 	unsigned int	nbr;
 	
+	if (nb < 0)
+	{
+		ft_putchar('-');
+		nbr = -nb;
+	}
+	else
+		nbr = nb;
+	if (nbr >= 10)
+	{
+		ft_putnbr(nbr / 10);
+		ft_putnbr(nbr % 10);
+	}
+	if (nbr < 10)
+		ft_putchar(nbr + '0');
 }
 
 int	main()
 {
 	int		nb;
 
+	nb = -2147483648;
+	ft_putnbr(nb);
+	ft_putchar('\n');
+	nb = 0;
+	ft_putnbr(nb);
+	ft_putchar('\n');
+	nb = 2147483647;
+	ft_putnbr(nb);
+	ft_putchar('\n');
+	return (0);
 }
