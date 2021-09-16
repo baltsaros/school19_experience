14d02807bdcdfc6943d3fcd88439e8851332d74f
abuzdin <abuzdin@student.s19.be>
2021-09-16 21:53:38 +0000

    ex03

---
+++ bc05/ex03/ft_recursive_power.c
@@ -0,0 +1,18 @@
+int		ft_recursieve_power(int nb, int power)
+{
+	int	n;
+
+	n = nb;
+	if (power < 0)
+		return (0);
+	if (power == 0)
+		return (1);
+		
+}
+
+#include <stdio.h>
+int		main()
+{
+	printf("%d\n". ft_recursieve_power(1024, 2));
+	return (0);
+}
