98e725b8acbed0e82add45129dd6066a531f4586
abuzdin <abuzdin@student.s19.be>
2021-09-17 09:29:15 +0000

    ex03 is done

--- bc05/ex03/ft_recursive_power.c
+++ bc05/ex03/ft_recursive_power.c
@@ -2,17 +2,27 @@ int		ft_recursieve_power(int nb, int power)
 {
 	int	n;
 
-	n = nb;
-	if (power < 0)
-		return (0);
+	if (power > 0)
+	{
+		n = nb * ft_recursieve_power(nb, --power);
+		return (n);
+	}
 	if (power == 0)
 		return (1);
+	else
+		return (0);
 		
 }
 
 #include <stdio.h>
 int		main()
 {
-	printf("%d\n". ft_recursieve_power(1024, 2));
+	printf("%d\n", ft_recursieve_power(3, -2));
+	printf("%d\n", ft_recursieve_power(3, 0));
+	printf("%d\n", ft_recursieve_power(3, 1));
+	printf("%d\n", ft_recursieve_power(3, 2));
+	printf("%d\n", ft_recursieve_power(3, 3));
+	printf("%d\n", ft_recursieve_power(3, 4));
+	printf("%d\n", ft_recursieve_power(3, 5));
 	return (0);
 }
