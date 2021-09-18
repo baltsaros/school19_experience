8b8673b62adc93255327dfde648c048fd92c0fa2
abuzdin <abuzdin@student.s19.be>
2021-09-18 21:28:45 +0000

    ex06 is done

--- bc05/ex06/ft_is_prime.c
+++ bc05/ex06/ft_is_prime.c
@@ -1,10 +1,33 @@
-int	ft_is_prime(int nb)
+int	ft_prime(int n, int i)
 {
-
+	if (n % i == 0)
+		return(0);
+	if (i * i > n)
+		return(1);
+	else
+		return(ft_prime(n, i + 1));
 }
 
+int	ft_is_prime(int nb)
+{
+	if (nb > 2)
+		return(ft_prime(nb, 2));
+	if (nb == 2)
+		return (1);
+	else
+		return (0);
+}
+
+#include <stdio.h>
 int	main()
 {
+	int		nb;
 
+	nb = 100;
+	while (nb > -3)
+	{
+		printf("%d --- %d\n", nb, ft_is_prime(nb));
+		--nb;
+	}
 	return (0);
 }
