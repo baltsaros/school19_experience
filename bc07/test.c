0d1f864b44d6a232ffe5bf91b58d7a6a41339be3
abuzdin <abuzdin@student.s19.be>
2021-09-20 08:13:08 +0000

    ex00 is done

---
+++ bc07/test.c
@@ -0,0 +1,32 @@
+#include <stdlib.h> 
+char	*ft_strdup(char *src)
+{
+	char	*dest;
+	int		i;
+	int		size;
+
+	size = 0;
+	while (src[size])
+		++size;
+	dest = malloc(sizeof(*src) * (size + 1));
+	if (dest == NULL)
+		return NULL;
+	i = 0;
+	while (src[i])
+	{
+		dest[i] = src[i];
+		++i;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+#include <stdio.h>
+#include <string.h>
+int		main()
+{
+	char	src[] = "String to copy";
+
+	printf("dest is %s\n", ft_strdup(src));
+	return (0);
+}
