806d733b045d06e8193606c8cd5f0aa8c2da99a4
abuzdin <abuzdin@student.s19.be>
2021-09-21 20:44:17 +0000

    update

---
+++ bc08/ex04/ft_strs_to_tab.c
@@ -0,0 +1,4 @@
+struct	s_stock_str *ft_strs_to_tab(int ac, char **av)
+{
+
+}
---
+++ bc08/ex04/ft_strs_to_tab.h
@@ -0,0 +1,10 @@
+#ifndef FT_STRS_TO_TAB_H
+# define FT_STRS_TO_TAB_H
+
+typedef struct s_stock_str
+{
+	int		size;
+	char	*str;
+	char	*copy;
+}	t_stock_str;
+#endif
