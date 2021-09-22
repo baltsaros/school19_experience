616c1d64120f521fe1818f6cfc50ac516511931e
abuzdin <abuzdin@student.s19.be>
2021-09-22 13:06:34 +0000

    update

---
+++ bc08/ex02/ft_abs.c
@@ -0,0 +1,9 @@
+#include "ft_abs.h"
+#include <stdio.h>
+
+int main()
+{
+	printf("%d\n", ABS(-42));
+	printf("%d\n", ABS(0));
+	printf("%d\n", ABS(42));
+}
--- bc08/ex04/ft_strs_to_tab.c
+++ bc08/ex04/ft_strs_to_tab.c
@@ -1,4 +1,69 @@
-struct	s_stock_str *ft_strs_to_tab(int ac, char **av)
-{
+#include <stdlib.h>
+#include "ft_strs_to_tab.h"
 
+int	ft_strlen(char *str)
+{
+	int		i;
+
+	i = 0;
+	while (str[i])
+		++i;
+	return (i);
+}
+
+char	*ft_strdup(char *str)
+{
+	int		j;
+	int		length;
+	char	*copy;
+
+	length = ft_strlen(str);
+	copy = (char *)malloc(sizeof(copy) * (length + 1));
+	if (NULL == copy)
+		return (NULL);
+	j = 0;	
+	while (str[j])
+	{
+		copy[j] = str[j];
+		++j;
+	}
+	copy[j] = '\0';
+	return (copy);
+}
+
+struct s_stock_str	*ft_strs_to_tab(int ac, char **av)
+{
+	t_stock_str		*stocks;
+	int				i;
+
+	stocks = malloc(sizeof(*stocks) * ac);
+	if (NULL == stocks)
+		return (NULL);
+	i = 0;
+	while (i < ac)
+	{
+		stocks[i].size = ft_strlen(av[i]);
+		stocks[i].str = av[i];
+		stocks[i].copy = ft_strdup(av[i]);
+		++i;
+	}
+	stocks[i].str = 0;
+	return (stocks);
+}
+
+#include <stdio.h>
+int	main(int argc, char **argv)
+{
+	int		k;
+	struct s_stock_str	*results;
+
+	results = ft_strs_to_tab(argc, argv);
+	k = 0;
+	while (k < argc)
+	{
+		printf("argc - %d\n", k);
+		printf("size %d | str%d %s | copy %s\n", results[k].size, k, results[k].str, results[k].copy);
+		++k;
+	}
+	return (0);
 }
