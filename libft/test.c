147448ca48326157e95a10e1997d562b93c735ed
baltsaros <buzdin.alexander@gmail.com>
2021-09-30 19:08:22 +0200

    update

---
+++ libft/test.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+  
+void printArray(int arr[], int n)
+{
+   for (int i=0; i<n; i++)
+      printf("%d ", arr[i]);
+}
+  
+int main()
+{
+    int n = 10;
+    int arr[n];
+  
+    // Fill whole array with 100.
+    memset(arr, -1, n*sizeof(arr[0]));
+    printf("Array after memset()\n");
+    printArray(arr, n);
+  
+    return 0;
+}
