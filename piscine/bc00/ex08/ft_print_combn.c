9c93f5c247b7db9a620cfcd533569cfb6b3eeea9
baltsaros <buzdin.alexander@gmail.com>
2021-11-01 12:12:52 +0100

    bc00/ex08 is done

--- piscine/bc00/ex08/ft_print_combn.c
+++ piscine/bc00/ex08/ft_print_combn.c
@@ -5,13 +5,60 @@ void	ft_putchar(char c)
 	write(1, &c, 1);
 }
 
+void	print_array(int *array, int n)
+{
+	int		i;
+
+	i = 0;
+	while (i < n)
+	{
+		ft_putchar(array[i] + 48);
+		++i;
+	}
+	if (array[0] != 10 - n)
+		write(1, ", ", 2);
+}
+
+void	make_combo(int *array, int n)
+{
+	int		i;
+
+	while (1)
+	{
+		i = 0;
+		while (array[n - 1] < 9)
+		{
+			array[n - 1] += 1;
+			print_array(array, n);
+		}
+		while (array[n - 1 - i] == 9 - i && i < n)
+			++i;
+		if (i == n)
+			break;
+		array[n - 1 - i] += 1;
+		array[n - i] = array[n - 1 - i];
+	}
+}
+
 void	ft_print_combn(int n)
 {
+	int		array[9];
+	int		i;
 
+	i = 0;
+	while (i < n)
+	{
+		array[i] = i;
+		ft_putchar(array[i] + 48);
+		++i;
+	}
+	write(1, ", ", 2);
+	make_combo(array, n);
 }
 
 int	main(void)
 {
-	ft_print_combn(3);
+	ft_print_combn(2);
+	write(1, "\n", 1);
 	return (0);
 }
--- piscine/bc00/test.c
+++ piscine/bc00/test.c
@@ -1,23 +1,77 @@
 #include <unistd.h>
+#include <stdbool.h>
 
-void	ft_putchar(char n)	
+void	ft_putchar(char c)
 {
-	write(1, &n, 1);
+	write(1, &c, 1);
 }
 
-void ft_print_alphabet(void)
+void	ft_write_combo(int n, int holders[])
 {
-	char	n;
-	char	a;
-	n = 0;
-	a = n / 10;
-	ft_putchar(a);
-	a = n / 10 + '0';
-	ft_putchar(a);
+	int		index;
+	bool	last;
+
+	index = 0;
+	while (index < n)
+	{
+		ft_putchar(48 + holders[index]);
+		index++;
+	}
+	index = n - 1;
+	last = true;
+	while (index >= 0)
+	{
+		if (holders[index] != 9 - (n - 1 - index))
+		{
+			last = false;
+			break ;
+		}
+		index--;
+	}
+	if (!last)
+	{
+		ft_putchar(',');
+		ft_putchar(' ');
+	}
 }
 
-int	main(void)
+void	ft_print_combn_recursive(int n, int curr, int holders[], int st_index)
 {
-	ft_print_alphabet();
-	return 0;
+	int index;
+	int max;
+
+	if (curr == n)
+	{
+		ft_write_combo(n, holders);
+	}
+	else
+	{
+		max = 10 - (n - curr);
+		index = st_index + 1;
+		while (index <= max)
+		{
+			holders[curr] = index;
+			ft_print_combn_recursive(n, curr + 1, holders, index);
+			index++;
+		}
+	}
+}
+
+void	ft_print_combn(int n)
+{
+	int holders[10];
+	int index;
+
+	index = 0;
+	while (index < n)
+	{
+		holders[index] = 0;
+		index++;
+	}
+	ft_print_combn_recursive(n, 0, holders, -1);
+}
+
+int		main(void)
+{
+	ft_print_combn(3);
 }
