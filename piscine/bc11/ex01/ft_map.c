810ddab4853bc43d0251f223864d786e9bf2ee27
baltsaros <buzdin.alexander@gmail.com>
2021-11-10 18:34:18 +0100

    ex02 is done

--- piscine/bc11/ex01/ft_map.c
+++ piscine/bc11/ex01/ft_map.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int	*ft_map(int *tab, int length, int(*f)(int))
+int	*ft_map(int *tab, int length, int (*f)(int))
 {
 	int		*ret;
 	int		i;
---
+++ piscine/bc11/ex02/ft_any.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int	ft_any(char **tab, int (*f)(char *))
+{
+	int		i;
+
+	i = 0;
+	while (tab[i] != 0)
+	{
+		if ((*f)(tab[i]) > 0)
+			return (1);
+		++i;
+	}
+	return (0);
+}
+
+int	ft_str_is_lowercase(char *str)
+{
+	int		i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] < 96 || str[i] > 122)
+			return (0);
+		++i;
+	}
+	return (1);
+}
+
+int	main(int argc, char *argv[])
+{
+	// argv[0] = "one";
+	// argv[1] = "two";
+	// argv[2] = "three";
+	// argv[3] = "Tfour";
+	// argv[4] = 0;
+	// (void)argc;
+	// // argc = 4;
+	if (argc > 1)
+		printf("return is %d\n", ft_any(argv, &ft_str_is_lowercase));
+	return (0);
+}
