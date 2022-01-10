47377c57f25a521aa72d0c23fe28aff40854c93f
Aleksandr Buzdin <abuzdin@fu-r2-p1.s19.be>
2022-01-10 10:54:28 +0100

    update

--- libft/libft/ft_split.c
+++ libft/libft/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: abuzdin <abuzdin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/10 09:33:11 by abuzdin           #+#    #+#             */
-/*   Updated: 2022/01/10 10:32:11 by abuzdin          ###   ########.fr       */
+/*   Updated: 2022/01/10 10:54:09 by abuzdin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,11 +48,13 @@ static char	*ft_strndup(char const *str, size_t size)
 	return (dest);
 }
 
-static char	*ft_create_str(char **spl, char const *s, char c)
+static char	**ft_create_str(char **spl, char const *s, char c)
 {
 	int		start;
 	int		end;
+	int		i;
 
+	i = 0;
 	start = 0;
 	while (s[start])
 	{
@@ -61,55 +63,51 @@ static char	*ft_create_str(char **spl, char const *s, char c)
 			++end;
 		if (end > 0)
 		{
-			*spl = ft_strndup(s + start, end);
+			spl[i] = ft_strndup(s + start, end);
+			++i;
 			start = start + end;
 		}
 		if (s[start])
 			++start;
 	}
-	return (*spl);
+	spl[i] = NULL;
+	return (spl);
 }
 
 char	**ft_split(char const *s, char c)
 {
 	char	**spl;
-	int		i;
 	int		size;
 
-	i = 0;
 	size = check_str(s, c);
 	if (!s)
 		return (NULL);
-	spl = (char **)malloc(sizeof(spl) * (i + 1));
+	spl = (char **)malloc(sizeof(spl) * (size + 1));
 	if (!spl)
 		return (NULL);
-	while (i < size)
-	{
-		spl[i] = ft_create_str(&spl[i], s, c);
-		++i;
-	}
-	spl[i] = NULL;
+	spl = ft_create_str(spl, s, c);
+	spl[size] = NULL;
 	return (spl);
 }
 
-int		main(void)
-{
-	// char	s[] = "b";
-	char	s[] = "aaabcaAaAaefaa a";
-	// char	s[] = "bac";
-	char	c;
-	char	**ret;
-	int		j;
+// int		main(void)
+// {
+// 	// char	s[] = "b";
+// 	// char	s[] = "aaabcaAaAaefaa a";
+// 	char	s[] = "bac";
+// 	char	c;
+// 	char	**ret;
+// 	int		j;
 
-	c = 'a';
-	ret = ft_split(s, c);
-	j = 0;
-	while (ret[j])
-	{
-		printf("str[%d] is '%s'\n", j, ret[j]);
-		++j;
-	}
-	printf("Final amount of strings is %d\n", j);
-	free(ret);
-	return (0);
-}
+// 	c = 'a';
+// 	ret = ft_split(s, c);
+// 	j = 0;
+// 	while (ret[j])
+// 	{
+// 		printf("str[%d] is '%s'\n", j, ret[j]);
+// 		++j;
+// 	}
+// 	printf("Final amount of strings is %d\n", j);
+// 	free(ret);
+// 	return (0);
+// }
