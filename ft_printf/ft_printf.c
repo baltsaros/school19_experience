e3ceb510fbc575f98ed28cd2bc756c72b196e512
Aleksandr Buzdin <abuzdin@fu-r2-p1.s19.be>
2022-01-20 14:46:49 +0100

    sorting

---
+++ ft_printf/ft_printf.c
@@ -0,0 +1,6 @@
+#include "ft_printf.h"
+
+int	ft_printf(const char *, ...)
+{
+	return (0);
+}
---
+++ ft_printf/ft_printf.h
@@ -0,0 +1,10 @@
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+#include <unistd.h>
+#include <stdarg.h>
+#include <stdlib.h>
+
+int	ft_printf(const char *, ...);
+
+#endif
