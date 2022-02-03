2e8b8af21ce892c23157886db3f34411b6019889
Aleksandr Buzdin <abuzdin@fu-r2-p1.s19.be>
2022-02-03 18:01:00 +0100

    update

--- get_next_line/alanorm/get_next_line.h
+++ get_next_line/alanorm/get_next_line.h
@@ -6,12 +6,12 @@
 /*   By: abuzdin <abuzdin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/03 10:45:28 by abuzdin           #+#    #+#             */
-/*   Updated: 2022/02/03 11:54:56 by abuzdin          ###   ########.fr       */
+/*   Updated: 2022/02/03 18:00:47 by abuzdin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
-# define GET NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
 # include <unistd.h>
 # include <stdlib.h>
--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,12 +6,12 @@
 /*   By: abuzdin <abuzdin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/03 10:45:28 by abuzdin           #+#    #+#             */
-/*   Updated: 2022/02/03 11:13:48 by abuzdin          ###   ########.fr       */
+/*   Updated: 2022/02/03 18:00:32 by abuzdin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
-# define GET NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 42
