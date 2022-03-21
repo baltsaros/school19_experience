1baa039c58fa514a281ceaf2cd65a815c9210b62
baltsaros <abuzdin@student.s19.be>
2022-03-21 18:26:19 +0100

    upd

--- fractol_linux/main.c
+++ fractol_linux/main.c
@@ -28,6 +28,21 @@ typedef struct s_rect
 	int color;
 }	t_rect;
 
+typedef struct s_set
+{
+	double	pr;
+	double	pi;
+	double	newRe;
+	double	newIm;
+	double	oldRe;
+	double	oldIm;
+	double	zoom;
+	double	moveX;
+	double	moveY;
+	int		color;
+	int		maxIter;
+}	t_set;
+
 typedef struct s_round
 {
 	int	x;
@@ -49,6 +64,14 @@ enum {
 	ON_DESTROY = 17
 };
 
+void	init_set(t_set *mb)
+{
+	mb->zoom = 1;
+	mb->moveX = -0.5;
+	mb->moveY = 0;
+	mb->maxIter = 50;
+	mb->color = 10000;
+}
 
 int	key_hook(int keycode, t_data *data)
 {
@@ -116,6 +139,45 @@ int	render_rect(t_img *img, t_rect rect)
 	return (0);
 }
 
+int	render_mandelbrot(t_img *img, t_set mb)
+{
+	int	x;
+	int	y;
+	int	i;
+	int	iter;
+	int	color;
+
+	y = 0;
+	iter = 200;
+	color = 9999;
+	while (y < 800)
+	{
+		x = 0;
+		while (x < 900)
+		{
+			mb.pr = 1.5 * (x - 900 / 2) / (0.5 * mb.zoom * 900) + mb.moveX;
+			mb.pi = (y - 800 / 2) / (0.5 * mb.zoom * 800) + mb.moveY;
+			mb.newRe = mb.newIm = mb.oldRe = mb.oldIm = 0;
+			i = 0;
+			while (i < iter)
+			{
+				mb.oldRe = mb.newRe;
+				mb.oldIm = mb.newIm;
+				mb.newRe = mb.oldRe * mb.oldRe - mb.oldIm + mb.pr;
+				mb.newIm = 2 * mb.oldRe * mb.oldIm + mb.pi;
+				if ((mb.newRe * mb.newRe + mb.newIm * mb.newIm) > 4)
+					break ;
+				color += 250;
+				my_mlx_pixel_put(img, x, y, color);
+				++i;
+			}
+			++x;
+		}
+		++y;
+	}
+	return (0);
+}
+
 int	render_round(t_img *img, t_round round)
 {
 	int	i;
@@ -138,10 +200,10 @@ void	render_background(t_img *img, int color)
 	int	j;
 
 	i = 0;
-	while (i < 1080)
+	while (i < 800)
 	{
 		j = 0;
-		while (j < 1920)
+		while (j < 900)
 			my_mlx_pixel_put(img, j++, i, color);
 		++i;
 	}
@@ -149,14 +211,17 @@ void	render_background(t_img *img, int color)
 
 int	render(t_data *data)
 {
+	t_set	mandelbrot;
+
 	if (data->win == NULL)
 		return (1);
 	render_background(&data->img, 0xFFFFFF);
 	// render_rect(&data->img, (t_rect){0, 0, 100, 100, 0xFF00});
 	// render_rect(&data->img, (t_rect){1720, 880, 200, 200, 0xFF0000});
 	// render_rect(&data->img, (t_rect){500, 500, 200, 200, 203569230});
-	
-	render_rect(&data->img, (t_rect){500, 500, 200, 200, 203569230});
+	// render_rect(&data->img, (t_rect){500, 500, 200, 200, 203569230});
+	init_set(&mandelbrot);
+	render_mandelbrot(&data->img, mandelbrot);
 	mlx_put_image_to_window(data->mlx, data->win, data->img.mlx_img, 0, 0);
 	return (0);
 }
@@ -168,13 +233,13 @@ int	main(void)
 	data.mlx = mlx_init();
 	if (!data.mlx)
 		return (1);
-	data.win = mlx_new_window(data.mlx, 1920, 1080, "Test01");
+	data.win = mlx_new_window(data.mlx, 900, 800, "Test01");
 	if (!data.win)
 	{
 		free(data.win);
 		return (1);
 	}
-	data.img.mlx_img = mlx_new_image(data.mlx, 1920, 1080);
+	data.img.mlx_img = mlx_new_image(data.mlx, 900, 800);
 	data.img.addr = mlx_get_data_addr(data.img.mlx_img, &data.img.bpp, &data.img.line_length, &data.img.endian);
 	mlx_loop_hook(data.mlx, &render, &data);
 	mlx_hook(data.win, 2, 3, key_hook, &data);
