6943953821163b30e7e04a73a93e3254fe0895d3
Aleksandr Buzdin <abuzdin@shi-r6-p4.s19.be>
2022-05-16 09:54:27 +0200

    added structures and some external functions

--- minishell/minishell.h
+++ minishell/minishell.h
@@ -14,13 +14,58 @@
 # include <readline/readline.h>
 # include <readline/history.h>
 
+// # define SEPAR 1
+// # define WORD 2
+// # define QUOTE 3
+// # define QUOTE_D 4
+// # define REDIR_OUT 5
+// # define REDIR_IN 6
+// # define REDIR_AP 7
+// # define REDIR_HD 8
+// # define PIPE 9
+
+enum tokens
+{
+	SEPAR		= 1,
+	WORD		= 2,
+	QUOTE		= 3,
+	QUOTE_D		= 4,
+	REDIR_OUT	= 5,
+	REDIR_IN	= 6,
+	REDIR_AP	= 7,
+	REDIR_HD	= 8,
+	PIPE		= 9
+};
+
+typedef struct s_node
+{
+	int				type;
+	char			*value;
+	struct s_node	*next;
+	struct s_node	*prev;
+}	t_node;
+
 typedef struct s_input
 {
 	int		argc;
 	char	**argv;
-	char	**env;
+	char	**envp;
+	t_node	envp_n;
 	char	*buf;
 }	t_input;
+
+
+// allocation check
+void	alloc_check(char **str);
+void	alloc_check_small(void *str);
+void	alloc_check_node(t_node *new, t_node **node);
+
+// nodes
+t_node	*ft_node_new(int type, char *value);
+void	ft_node_back(t_node **node, t_node *new);
+t_node	*ft_node_del(t_node *node);
+void	ft_node_print(t_node *node);
+int		ft_node_size(t_node *node);
 
 
 #endif
---
+++ minishell/nodes_1.c
@@ -0,0 +1,109 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   nodes_1.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abuzdin <abuzdin@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/14 17:36:47 by abuzdin           #+#    #+#             */
+/*   Updated: 2022/05/16 09:40:17 by abuzdin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+t_node	*ft_node_new(int type, char *value)
+{
+	t_node	*node;
+
+	node = malloc(sizeof(t_node));
+	alloc_check_small(node);
+	node->type = type;
+	node->value = value;
+	node->next = node;
+	node->prev = node;
+	return (node);
+}
+
+void	ft_node_back(t_node **node, t_node *new)
+{
+	if (!node || !new)
+		return ;
+	if (!*node)
+		*node = new;
+	else
+	{
+		new->next = *node;
+		new->prev = (*node)->prev;
+		(*node)->prev->next = new;
+		(*node)->prev = new;
+	}
+}
+
+t_node	*ft_node_del(t_node *node)
+{
+	t_node	*tmp;
+
+	if (!node)
+		return (NULL);
+	tmp = node;
+	if (tmp == tmp->next)
+		node = NULL;
+	else if (tmp->prev == tmp->next)
+	{
+		node = node->next;
+		node->prev = node;
+		node->next = node;
+	}
+	else
+	{
+		node->prev->next = node->next;
+		node->next->prev = node->prev;
+		node = node->next;
+	}
+	free(tmp);
+	return (node);
+}
+
+void	ft_node_print(t_node *node)
+{
+	int		i;
+	int		len;
+	t_node	*head;
+
+	if (!node)
+	{
+		printf("There are no nodes in the list\n");
+		return ;
+	}
+	head = node;
+	i = 0;
+	len = ft_node_size(head);
+	printf("===== NODE =====\n");
+	while (i < len)
+	{
+		printf("type for node[%d] is %d\n", i, head->type);
+		printf("value for node[%d] is %s\n", i, head->value);
+		head = head->next;
+		++i;
+	}
+}
+
+int	ft_node_size(t_node *node)
+{
+	int		i;
+	t_node	*last;
+	t_node	*tmp;
+
+	if (!node)
+		return (0);
+	i = 1;
+	tmp = node;
+	last = tmp->prev;
+	while (tmp != last)
+	{
+		++i;
+		tmp = tmp->next;
+	}
+	return (i);
+}
