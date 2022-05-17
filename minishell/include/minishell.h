cc2cd3ba22e34d5f18092f745367f3509b1122bd
Aleksandr Buzdin <abuzdin@fu-r1-p4.s19.be>
2022-05-17 14:31:26 +0200

    small update

--- minishell/include/minishell.h
+++ minishell/include/minishell.h
@@ -13,7 +13,7 @@
 # include <term.h>
 # include <readline/readline.h>
 # include <readline/history.h>
-# include <libft.h>
+// # include <libft.h>
 // # include "libft/libft.h"
 
 // # define SEPAR 1
--- minishell/test.c
+++ minishell/test.c
@@ -1,49 +1,49 @@
 #include "include/minishell.h"
 
-void	create_envp(t_input *data, char *envp[])
-{
-	int		i;
-	int		start;
-	int		end;
-	char	*type;
-	char	*value;
-	t_env	*tmp;
+// void	create_envp(t_input *data, char *envp[])
+// {
+// 	int		i;
+// 	int		start;
+// 	int		end;
+// 	char	*type;
+// 	char	*value;
+// 	t_env	*tmp;
 
-	i = 0;
-	while (envp[i])
-	{
-		start = 0;
-		end = 0;
-		while (envp[i][start] != '=')
-			++start;
-		while (envp[i][end])
-			++end;
-		printf("strndup\n");
-		type = ft_strndup(envp[i], start + 1);
-		value = ft_strndup(envp[i] + start + 1, end - start - 1);
-		printf("new list\n");
-		tmp = ft_envp_new(type, value);
-		printf("list back\n");
-		ft_envp_back(&data->envp_n, tmp);
-		++i;
-	}
-}
+// 	i = 0;
+// 	while (envp[i])
+// 	{
+// 		start = 0;
+// 		end = 0;
+// 		while (envp[i][start] != '=')
+// 			++start;
+// 		while (envp[i][end])
+// 			++end;
+// 		printf("strndup\n");
+// 		type = ft_strndup(envp[i], start + 1);
+// 		value = ft_strndup(envp[i] + start + 1, end - start - 1);
+// 		printf("new list\n");
+// 		tmp = ft_envp_new(type, value);
+// 		printf("list back\n");
+// 		ft_envp_back(&data->envp_n, tmp);
+// 		++i;
+// 	}
+// }
 
-void	data_init(t_input *data, int argc, char *argv[], char *envp[])
-{
-	data->argc = argc;
-	data->argv = argv;
-	data->envp = envp;
-	data->status = 0;
-	data->envp_n = NULL;
-	create_envp(data, envp);
-	printf("node print\n");
-	ft_envp_print(data->envp_n);
-	printf("node del\n");
-	data->envp_n = ft_free_envp(data->envp_n);
-	printf("node print\n");
-	ft_envp_print(data->envp_n);
-}
+// void	data_init(t_input *data, int argc, char *argv[], char *envp[])
+// {
+// 	data->argc = argc;
+// 	data->argv = argv;
+// 	data->envp = envp;
+// 	data->status = 0;
+// 	data->envp_n = NULL;
+// 	create_envp(data, envp);
+// 	printf("node print\n");
+// 	ft_envp_print(data->envp_n);
+// 	printf("node del\n");
+// 	data->envp_n = ft_free_envp(data->envp_n);
+// 	printf("node print\n");
+// 	ft_envp_print(data->envp_n);
+// }
 
 int	main(int argc, char *argv[], char *envp[])
 {
@@ -52,13 +52,8 @@ int	main(int argc, char *argv[], char *envp[])
 
 	// data_init(&data, argc, argv, envp);
 	i = 0;
-	data.argv = ft_split_op("one \" two     \" three \"four\"", ' ');
-	while (data.argv[i])
-	{
-		printf("argv[%d] is |%s|\n", i, data.argv[i]);
-		++i;
-	}
-	ft_free(data.argv);
+	data.buf = malloc(5);
+	exit(0);
 	// system("leaks out");
 	return (0);
 }
