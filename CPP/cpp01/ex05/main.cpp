4e6426c3fbe55ecd0601f9dd806add482b362098
Aleksandr Buzdin <buzdin.alexander@gmail.com>
2022-06-21 12:44:02 +0200

    added some colors

--- CPP/cpp01/ex05/main.cpp
+++ CPP/cpp01/ex05/main.cpp
@@ -1,24 +1,29 @@
 #include "Harl.hpp"
 
+#define NC "\e[0m"
+#define RED "\e[0;31m"
+#define REDB "\e[41m"
+
 int	main(void){
 	Harl	customer;
 
 	std::cout << "TESTING!" << std::endl;
 	std::cout << std::endl;
 
-	std::cout << "Level: debug" << std::endl;
+
+	std::cout << "Level: " << RED "debug" NC << std::endl;
 	customer.complain("DEBUG");
 	std::cout << std::endl;
-	
-	std::cout << "Level: info" << std::endl;
+
+	std::cout << "Level: " << RED "info" NC << std::endl;
 	customer.complain("INFO");
 	std::cout << std::endl;
 	
-	std::cout << "Level: warning" << std::endl;
+	std::cout << "Level: " <<  RED "warning" NC << std::endl;
 	customer.complain("WARNING");
 	std::cout << std::endl;
 	
-	std::cout << "Level: error" << std::endl;
+	std::cout << "Level: " <<  RED "error" NC << std::endl;
 	customer.complain("ERROR");
 	std::cout << std::endl;
 
