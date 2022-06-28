04063256de9bebcb7ec0d56a731a8639c5b7c3d7
Aleksandr Buzdin <buzdin.alexander@gmail.com>
2022-06-28 16:56:29 +0200

    small fix

--- CPP/cpp04/ex00/Cat.hpp
+++ CPP/cpp04/ex00/Cat.hpp
@@ -1,25 +1,6 @@
 #ifndef CAT_HPP
 # define CAT_HPP
 
-# define NC "\e[0m"
-# define GRNB "\e[1;32m"
-# define GRN "\e[0;32m"
-# define RED "\e[0;31m"
-# define REDB "\e[1;31m"
-# define REDL "\e[41m"
-# define CYN "\e[0;36m"
-# define CYNB "\e[1;36m"
-# define YLW "\e[0;33m"
-# define YLWB "\e[1;33m"
-# define PRPL "\e[0;35m"
-# define PRPLB "\e[1;35m"
-# define WHT "\e[0;37m"
-# define WHTB "\e[1;37m"
-# define WHTL "\e[37m"
-# define BLUE "\e[0;34m"
-# define BLUEB "\e[1;34m"
-# define BLUEL "\e[34m"
-
 # include <iostream>
 # include <string>
 # include "Animal.hpp"
