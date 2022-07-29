baf5e201478496884fbe5e9aa1705250e5463081
Aleksandr Buzdin <buzdin.alexander@gmail.com>
2022-07-29 16:46:35 +0200

    extra tests

--- CPP/cpp07/ex00/main.cpp
+++ CPP/cpp07/ex00/main.cpp
@@ -4,6 +4,21 @@
 
 int	main(void) {
 	std::cout << std::endl;
+	std::cout << YLWB "Tests from the subject" NC << std::endl;
+	int a = 2;
+	int b = 3;
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	std::cout << std::endl;
+
 
 	std::cout << YLWB "Testing swap" NC << std::endl;
 	int			n1 = 6;
@@ -46,9 +61,9 @@ int	main(void) {
 	std::cout << "c1 is " << c1 << ", c2 is " << c2 << std::endl;
 	std::cout << "min is " << min<char>(c1, c2) << std::endl;
 	std::cout << std::endl;
-	// std::cout << "s1 is " << s1 << ", s2 is " << s2 << std::endl;
-	// std::cout << "min is " << min(s1, s2) << std::endl;
-	// std::cout << "s1 is " << s1 << ", s2 is " << s2 << std::endl;
+	std::cout << "s1 is " << s1 << ", s2 is " << s2 << std::endl;
+	std::cout << "min is " << ::min(s1, s2) << std::endl;
+	std::cout << std::endl;
 
 	std::cout << YLWB "Testing max" NC << std::endl;
 	std::cout << "n1 is " << n1 << ", n2 is " << n2 << std::endl;
@@ -59,6 +74,9 @@ int	main(void) {
 	std::cout << std::endl;
 	std::cout << "c1 is " << c1 << ", c2 is " << c2 << std::endl;
 	std::cout << "max is " << max<char>(c1, c2) << std::endl;
+	std::cout << std::endl;
+	std::cout << "s1 is " << s1 << ", s2 is " << s2 << std::endl;
+	std::cout << "max is " << ::max(s1, s2) << std::endl;
 	std::cout << std::endl;
 
 	return (0);
