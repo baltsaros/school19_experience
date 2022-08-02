bd37f7ee74dc6f74cbd235f1ed94fec381f7eeeb
baltsaros <buzdin.alexander@gmail.com>
2022-08-02 18:46:30 +0200

    extra tests

--- CPP/cpp08/ex02/main.cpp
+++ CPP/cpp08/ex02/main.cpp
@@ -1,28 +1,84 @@
+#include <algorithm>
+#include <vector>
+#include <list>
 #include "MutantStack.hpp"
 
 int main()
 {
 	std::cout << YLWB "Test from the subject\n" NC;
-	MutantStack<int> mstack;
-	mstack.push(5);
-	mstack.push(17);
-	std::cout << mstack.top() << std::endl;
-	mstack.pop();
-	std::cout << mstack.size() << std::endl;
-	mstack.push(3);
-	mstack.push(5);
-	mstack.push(737);
-	//[...]
-	mstack.push(0);
-	MutantStack<int>::iterator it = mstack.begin();
-	MutantStack<int>::iterator ite = mstack.end();
-	++it;
-	--it;
-	while (it != ite)
 	{
-	std::cout << *it << std::endl;
-	++it;
+		MutantStack<int> mstack;
+		mstack.push(5);
+		mstack.push(17);
+		std::cout << mstack.top() << std::endl;
+		mstack.pop();
+		std::cout << mstack.size() << std::endl;
+		mstack.push(3);
+		mstack.push(5);
+		mstack.push(737);
+		//[...]
+		mstack.push(0);
+		MutantStack<int>::iterator it = mstack.begin();
+		MutantStack<int>::iterator ite = mstack.end();
+		++it;
+		--it;
+		while (it != ite)
+		{
+		std::cout << *it << std::endl;
+		++it;
+		}
+		std::stack<int> s(mstack);
+		std::cout << std::endl;
 	}
-	std::stack<int> s(mstack);
+
+	{
+		std::cout << YLWB "Extra test: vector\n" NC;
+		std::vector<int> vct;
+		vct.push_back(5);
+		vct.push_back(17);
+		std::cout << vct.front() << std::endl;
+		vct.pop_back();
+		std::cout << vct.size() << std::endl;
+		vct.push_back(3);
+		vct.push_back(5);
+		vct.push_back(737);
+		//[...]
+		vct.push_back(0);
+		std::vector<int>::iterator it = vct.begin();
+		std::vector<int>::iterator ite = vct.end();
+		++it;
+		--it;
+		while (it != ite)
+		{
+		std::cout << *it << std::endl;
+		++it;
+		}
+		std::cout << std::endl;
+	}
+
+	{
+		std::cout << YLWB "Extra test: list\n" NC;
+		std::list<int> lst;
+		lst.push_back(5);
+		lst.push_back(17);
+		std::cout << lst.front() << std::endl;
+		lst.pop_back();
+		std::cout << lst.size() << std::endl;
+		lst.push_back(3);
+		lst.push_back(5);
+		lst.push_back(737);
+		//[...]
+		lst.push_back(0);
+		std::list<int>::iterator it = lst.begin();
+		std::list<int>::iterator ite = lst.end();
+		++it;
+		--it;
+		while (it != ite)
+		{
+		std::cout << *it << std::endl;
+		++it;
+		}
+		std::cout << std::endl;
+	}
 	return 0;
 }
