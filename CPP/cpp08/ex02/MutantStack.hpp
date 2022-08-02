b2c5b46ccb5df566425da440f76e9c384a835ca7
baltsaros <buzdin.alexander@gmail.com>
2022-08-02 16:52:55 +0200

    ex02 beginning

---
+++ CPP/cpp08/ex02/MutantStack.hpp
@@ -0,0 +1,38 @@
+#ifndef MUTANTSTACK_HPP
+# define MUTANTSTACK_HPP
+
+# define NC "\e[0m"
+# define GRNB "\e[1;32m"
+# define GRN "\e[0;32m"
+# define RED "\e[0;31m"
+# define REDB "\e[1;31m"
+# define REDL "\e[41m"
+# define CYN "\e[0;36m"
+# define CYNB "\e[1;36m"
+# define YLW "\e[0;33m"
+# define YLWB "\e[1;33m"
+# define PRPL "\e[0;35m"
+# define PRPLB "\e[1;35m"
+# define WHT "\e[0;37m"
+# define WHTB "\e[1;37m"
+# define WHTL "\e[37m"
+# define BLUE "\e[0;34m"
+# define BLUEB "\e[1;34m"
+# define BLUEL "\e[34m"
+
+# include <iostream>
+# include <string>
+
+template <typename T>
+class	MutantStack {
+	private:
+
+	public:
+		MutantStack(void);
+		MutantStack(MutantStack const &src);
+		~MutantStack(void);
+
+		MutantStack&	operator=(MutantStack const &rhs);
+};
+
+#endif
