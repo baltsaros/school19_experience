d103209f6164a7311eeba3cd643abb3df30bd3ed
Aleksandr Buzdin <buzdin.alexander@gmail.com>
2022-08-30 14:36:16 +0200

    ready for push

--- CPP/cpp06/ex00/main.cpp
+++ CPP/cpp06/ex00/main.cpp
@@ -1,8 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abuzdin <abuzdin@student.s19.be>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/30 14:22:23 by abuzdin           #+#    #+#             */
+/*   Updated: 2022/08/30 14:27:30 by abuzdin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "Convertor.hpp"
 
 int	main(int argc, char *argv[]) {
 	if (argc != 2) {
-		std::cout << "Error: Invalid amout of arguments" << std::endl;
+		std::cout << "Error: Invalid amount of arguments" << std::endl;
 		return (1);
 	}
 	try {
--- CPP/cpp06/ex02/B.cpp
+++
@@ -1,3 +0,0 @@
-#include "B.hpp"
-
-//	OTHERS
