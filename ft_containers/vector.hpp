b14d861d8c5c45a251ef6f2ba25e659350a44bc2
baltsaros <abuzdin@student.s19.be>
2022-09-08 13:46:45 +0200

    it begins

---
+++ ft_containers/vector.hpp
@@ -0,0 +1,18 @@
+#ifndef VECTOR_HPP
+# define VECTOR_HPP
+
+# include <memory>
+
+namespace ft
+{
+	template <class T, class Allocator = std::allocator<T>>
+	class Vector {
+		private:
+
+		public:
+
+	}
+}
+
+
+#endif
