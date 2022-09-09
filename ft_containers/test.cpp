33d715cd61f87e2c74b28c8ed956d97e40e9ad4f
baltsaros <abuzdin@student.s19.be>
2022-09-09 12:24:32 +0200

    capacity is done, added tests

--- ft_containers/test.cpp
+++ ft_containers/test.cpp
@@ -25,43 +25,112 @@ int	main(void) {
 		// 	throw out_of_range("range error");
 	}
 
-	// ft_vector: access operators
-	{
-		std::vector<int>	v1 (4, 100);
-		ft::vector<int>		v2(4, 100);
-		ft::vector<int>		v4;
-		int					*p;
-
-		std::cout << "v1.at(0): " << v1.at(0) << std::endl;
-		std::cout << "v2.at(0): " << v2.at(0) << std::endl;
-		std::cout << "v1.at(3): " << v1.at(3) << std::endl;
-		std::cout << "v2.at(3): " << v2.at(3) << std::endl;
-		std::cout << "v1.at(2) = 3: " << (v1.at(2) = 3) << std::endl;
-		std::cout << "v1.at(2): " << v1.at(2) << std::endl;
-		std::cout << "v2.at(2) = 3: " << (v2.at(2) = 3) << std::endl;
-		std::cout << "v2.at(2): " << v2.at(2) << std::endl;
-		// std::cout << "v1.at(4): " << v1.at(4) << std::endl;
-		// std::cout << "v2.at(4): " << v2.at(4) << std::endl;
-		std::cout << "v1[0]: " << v1[0] << std::endl;
-		std::cout << "v2[0]: " << v2[0] << std::endl;
-		std::cout << "v1[2]: " << v1[2] << std::endl;
-		std::cout << "v2[2]: " << v2[2] << std::endl;
-		// std::cout << "v1[5]: " << v1[5] << std::endl;
-		// std::cout << "v2[5]: " << v2[5] << std::endl;
-		std::cout << "v1.front(): " << v1.front() << std::endl;
-		std::cout << "v2.front(): " << v2.front() << std::endl;
-		// std::cout << "EMPTY: v4.front(): " << v4.front() << std::endl;
-		std::cout << "v1.at(3) = 6: " << (v1.at(3) = 6) << std::endl;
-		std::cout << "v1.at(3): " << v1.at(3) << std::endl;
-		std::cout << "v2.at(3) = 6: " << (v2.at(3) = 6) << std::endl;
-		std::cout << "v2.at(3): " << v2.at(3) << std::endl;
-		std::cout << "v1.back(): " << v1.back() << std::endl;
-		std::cout << "v2.back(): " << v2.back() << std::endl;
-		std::cout << "p = v1.data(): " << (p = v1.data()) << std::endl;
-		std::cout << "p[3]: " << p[3] << std::endl;
-		std::cout << "p = v2.data(): " << (p = v2.data()) << std::endl;
-		std::cout << "p[3]: " << p[3] << std::endl;
-	}
+	// ft_vector: assign, allocator
+	// {
+	// 	std::vector<int>	v1 (4, 100);
+	// 	ft::vector<int>		v2(4, 100);
+	// 	int					*p;
 	
+	// 	v1.assign(2, 5);
+	// 	std::cout << "get_allocator: getting type for int array, allocate memory for it and fill it" << std::endl;
+	// 	p = v1.get_allocator().allocate(3);
+	// 	for (size_t i = 0; i < 3; ++i) {
+	// 		v1.get_allocator().construct(&p[i], i);
+	// 		std::cout << "std: " << p[i] << std::endl;
+	// 		v1.get_allocator().destroy(&p[i]);
+	// 	}
+	// 	v1.get_allocator().deallocate(p, 3);
+	// 	p = v2.get_allocator().allocate(3);
+	// 	for (size_t i = 0; i < 3; ++i) {
+	// 		v2.get_allocator().construct(&p[i], i);
+	// 		std::cout << "ft: " << p[i] << std::endl;
+	// 		v2.get_allocator().destroy(&p[i]);
+	// 	}
+	// 	v1.get_allocator().deallocate(p, 3);
+	// }
+
+	// ft_vector: access operators
+	// {
+	// 	std::vector<int>	v1(4, 100);
+	// 	ft::vector<int>		v2(4, 100);
+	// 	ft::vector<int>		v4;
+	// 	int					*p;
+
+	// 	std::cout << "v1.at(0): " << v1.at(0) << std::endl;
+	// 	std::cout << "v2.at(0): " << v2.at(0) << std::endl;
+	// 	std::cout << "v1.at(3): " << v1.at(3) << std::endl;
+	// 	std::cout << "v2.at(3): " << v2.at(3) << std::endl;
+	// 	std::cout << "v1.at(2) = 3: " << (v1.at(2) = 3) << std::endl;
+	// 	std::cout << "v1.at(2): " << v1.at(2) << std::endl;
+	// 	std::cout << "v2.at(2) = 3: " << (v2.at(2) = 3) << std::endl;
+	// 	std::cout << "v2.at(2): " << v2.at(2) << std::endl;
+	// 	// std::cout << "v1.at(4): " << v1.at(4) << std::endl;
+	// 	// std::cout << "v2.at(4): " << v2.at(4) << std::endl;
+
+	// 	std::cout << "v1[0]: " << v1[0] << std::endl;
+	// 	std::cout << "v2[0]: " << v2[0] << std::endl;
+	// 	std::cout << "v1[2]: " << v1[2] << std::endl;
+	// 	std::cout << "v2[2]: " << v2[2] << std::endl;
+	// 	// std::cout << "v1[5]: " << v1[5] << std::endl;
+	// 	// std::cout << "v2[5]: " << v2[5] << std::endl;
+
+	// 	std::cout << "v1.front(): " << v1.front() << std::endl;
+	// 	std::cout << "v2.front(): " << v2.front() << std::endl;
+	// 	// std::cout << "EMPTY: v4.front(): " << v4.front() << std::endl;
+
+	// 	std::cout << "v1.at(3) = 6: " << (v1.at(3) = 6) << std::endl;
+	// 	std::cout << "v1.at(3): " << v1.at(3) << std::endl;
+	// 	std::cout << "v2.at(3) = 6: " << (v2.at(3) = 6) << std::endl;
+	// 	std::cout << "v2.at(3): " << v2.at(3) << std::endl;
+	// 	std::cout << "v1.back(): " << v1.back() << std::endl;
+	// 	std::cout << "v2.back(): " << v2.back() << std::endl;
+
+	// 	std::cout << "p = v1.data(): " << (p = v1.data()) << std::endl;
+	// 	std::cout << "p[3]: " << p[3] << std::endl;
+	// 	std::cout << "p = v2.data(): " << (p = v2.data()) << std::endl;
+	// 	std::cout << "p[3]: " << p[3] << std::endl;
+	// }
+
+	// ft_vector: capacity
+	{
+		std::vector<int>	v1(4, 100);
+		ft::vector<int>		v2(4, 100);
+		std::vector<int>	v3;
+		ft::vector<int>		v4;
+
+		std::cout << "std: v1 empty? " << v1.empty() << std::endl;
+		std::cout << "ft: v2 empty? " << v2.empty() << std::endl;
+		std::cout << "std: v3 empty? " << v3.empty() << std::endl;
+		std::cout << "ft: v4 empty? " << v4.empty() << std::endl;
+
+		std::cout << "std: v1.size(): " << v1.size() << std::endl;
+		std::cout << "ft: v2.size(): " << v2.size() << std::endl;
+		std::cout << "std: v3.size(): " << v3.size() << std::endl;
+		std::cout << "ft: v4.size(): " << v4.size() << std::endl;
+
+		std::cout << "std: v1.max_size(): " << v1.max_size() << std::endl;
+		std::cout << "ft: v2.max_size(): " << v2.max_size() << std::endl;
+		std::cout << "std: v3.max_size(): " << v3.max_size() << std::endl;
+		std::cout << "ft: v4.max_size(): " << v4.max_size() << std::endl;
+
+		std::cout << "std: v1.reserve(150): " << std::endl;
+		v1.reserve(150);
+		std::cout << "v1.capacity(): " << v1.capacity() << std::endl;
+		std::cout << "v1[0]: " << v1[0] << std::endl;
+		std::cout << "v1[2]: " << v1[2] << std::endl;
+		std::cout << "ft: v2.reserve(150): " << std::endl;
+		v2.reserve(150);
+		std::cout << "v2.capacity(): " << v2.capacity() << std::endl;
+		std::cout << "v2[0]: " << v2[0] << std::endl;
+		std::cout << "v2[2]: " << v2[2] << std::endl;
+		// std::cout << "reserve(4611686018427387904): " << std::endl;
+		// v3.reserve(4611686018427387904);
+		// v4.reserve(4611686018427387904);
+		std::cout << "reserve(10) empty containers: " << std::endl;
+		v3.reserve(10);
+		std::cout << "v3.capacity(): " << v3.capacity() << std::endl;
+		v4.reserve(10);
+		std::cout << "v4.capacity(): " << v4.capacity() << std::endl;
+	}
 	return (0);
 }
--- ft_containers/vector.hpp
+++ ft_containers/vector.hpp
@@ -2,20 +2,27 @@
 # define VECTOR_HPP
 
 # include <memory>
-// # include <vector>
 # include <exception>
+# include <algorithm>
+# include <cstring>
+# include <vector>
 
 namespace ft {
 	template <class T, class Allocator = std::allocator<T> >
 	class vector {
+		public:
+			// TYPEDEFS
+			typedef std::size_t size_type;
+
 		private:
 			Allocator	_alloc;
-			size_t		_count, _cap;
+			size_type	_count, _cap;
 			T*			_head;
 
 
+
 		public:
-			// CONSTRUCTORS //
+			// CONSTRUCTORS
 			vector() :
 				_alloc(), _count(0), _cap(0), _head(0) {
 				return ;
@@ -26,27 +33,27 @@ namespace ft {
 				return ;
 			}
 
-			explicit vector(T count, const T& value = T(), const Allocator& alloc = Allocator()) :
+			explicit vector(size_t count, const T& value = T(), const Allocator& alloc = Allocator()) :
 				_alloc(alloc), _count(count), _cap(count) {
-				_head = _alloc.allocate(_count);
-				for (size_t i = 0; i < _count; ++i) {
-					_alloc.construct(_head + i, value);
+				this->_head = this->_alloc.allocate(this->_count);
+				for (size_t i = 0; i < this->_count; ++i) {
+					this->_alloc.construct(this->_head + i, value);
 				}
 				return ;
 			}
 
-			template <class InputIt>
-			vector(InputIt first, InputIt last, const Allocator& alloc = Allocator()) :
-				_alloc(alloc), _count(0), _cap(0), _head(0) {
-				return ;
-			}
+			// template <class InputIt>
+			// vector(InputIt first, InputIt last, const Allocator& alloc = Allocator()) :
+			// 	_alloc(alloc), _count(0), _cap(0), _head(0) {
+			// 	return ;
+			// }
 
 			vector(const vector& other) {
 				*this = other;
 				return ;
 			}
 
-			//	DESTRUCTOR //
+			//	DESTRUCTOR
 			~vector() {
 				for (size_t i = 0; i < this->_count; ++i) {
 					this->_alloc.destroy(this->_head + i);
@@ -55,7 +62,7 @@ namespace ft {
 				return ;
 			}
 
-			// ASSIGN OPERATOR //
+			// ASSIGN OPERATOR
 			vector&	operator=(const vector& other) {
 				if (this == &other)
 					return (*this);
@@ -73,8 +80,9 @@ namespace ft {
 				return (*this);
 			}
 
-			// ASSIGN //
-			void	assign(T count, const T& value){
+			// ASSIGN
+			void	assign(size_t count, const T& value){
+
 				return ;
 			}
 
@@ -83,26 +91,31 @@ namespace ft {
 				return ;
 			}
 
-			// ELEMENT ACCESS //
-			T& at(T pos) {
+			// GET_ALLOCATOR
+			Allocator	get_allocator() const {
+				return (this->_alloc);
+			}
+
+			// ELEMENT ACCESS
+			T& at(size_t pos) {
 				if (pos >= this->_count)
 					throw OutOfRange();
 				return (this->_head[pos]);
 			}
 
-			const T& at(T pos) const {
+			const T& at(size_t pos) const {
 				if (pos >= this->_count)
 					throw OutOfRange();
 				return (this->_head[pos]);
 			}
 
-			T&	operator[](T pos) {
+			T&	operator[](size_t pos) {
 				if (pos >= this->_count)
 					throw OutOfBounds();
 				return (this->_head[pos]);
 			}
 
-			const T&	operator[](T pos) const {
+			const T&	operator[](size_t pos) const {
 				if (pos >= this->_count)
 					throw OutOfBounds();
 				return (this->_head[pos]);
@@ -140,7 +153,45 @@ namespace ft {
 				return (this->_head);
 			}
 
+			// ITERATORS
+
+
+			// CAPACITY
+			bool empty() const {
+				return (this->_head == this->_head + this->_count);
+			}
+
+			size_t	size() const {
+				return (this->_count);
+			}
+
+			size_t	max_size() const {
+				return (this->_alloc.max_size());
+			}
+
+			void	reserve(size_t new_cap) {
+				if (new_cap <= this->_cap)
+					return ;
+				T	*tmp;
+				tmp = this->_alloc.allocate(new_cap);
+				for (size_t i = 0; i < this->_count; ++i) {
+					this->_alloc.construct(tmp + i, this->_head[i]);
+				}
+				for (size_t i = 0; i < this->_count; ++i) {
+					this->_alloc.destroy(this->_head + i);
+				}
+				this->_alloc.deallocate(this->_head, this->_count);
+				this->_cap = new_cap;
+				this->_head = tmp;
+				// INVALIDATE ITERATORS
+			}
+
+			size_t	capacity() const {
+				return (this->_cap);
+			}
+
 			// MODIFIERS
+
 
 			// EXCEPTIONS
 			class OutOfRange: public std::exception {
